0d93f17e62a64e03bdfd55707d062b51604ba3d5
mihret_mekbebe <mihretasamenew2018@gmail.com>
2023-03-21 02:22:29 -0700

    main

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,18 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+/**
+ * print_alphabet - print alphabet function
+ */
+
+void print_alphabet(void);
+
+/**
+ * print_alphabet_x10 - print alphabet 10 times function
+ */
+
+void print_alphabet_x10(void);
+
+
+#endif
+
