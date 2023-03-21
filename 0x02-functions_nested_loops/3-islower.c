da1adbb589be0494d42cd5dee03019a1e697953b
mihret_mekbebe <mihretasamenew2018@gmail.com>
2023-03-21 08:20:51 -0700

    3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _islower - checking if c is lower
+ * Return: 0 if false or 1 if true
+ */
+
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		Return(1);
+	Return(0);
+}
