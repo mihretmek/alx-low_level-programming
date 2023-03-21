5ccd7fb5ada0781e612fba017f6d3154e7943e62
mihret_mekbebe <mihretasamenew2018@gmail.com>
2023-03-21 02:11:52 -0700

    1-3

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include <main.h>
+
+/**
+ * print_alphabet - to print alphabets
+ * Return: 0
+ */
+
+void print_alphabet(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z'; a++)
+		_putchar(a);
+	_putchar('\n');
+	Return(0);
+}
---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,13 @@
+#include <main.h>
+
+/**
+ * _islower - checking if c is lower
+ * Return: 0 or 1
+ */
+
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		Return(1);
+	Return(0);
+}
