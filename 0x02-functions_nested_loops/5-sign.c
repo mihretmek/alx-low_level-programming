961403ae3f7c18eeeefc5b3fcdcc11aa5acd558f
mihret_mekbebe <mihretasamenew2018@gmail.com>
2023-03-21 09:41:24 -0700

    5-6

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -12,13 +12,11 @@ int print_sign(int n)
 	{
 		return (1);
 		_putchar ('+');
-	}
-	if (n == 0)
+	} else if (n == 0)
 	{
 		return (0);
 		_putchar ('0');
-	}
-	if (n < 0)
+	} else
 	{
 		return (-1);
 		_putchar ('-');
