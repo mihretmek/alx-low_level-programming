c20a780c6404eedf78534140f50f51fd0a5aeb0e
mihret_mekbebe <mihretasamenew2018@gmail.com>
2023-03-21 03:20:43 -0700

    2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include <main.h>
+
+/**
+ * print_alphabet_x10 - prints the alphabet 10 times
+ * Return: 0
+ */
+
+void print_alphabet_x10(void)
+{
+	char a;
+	char n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		for (a = 'a'; a <= 'z'; a++)
+		{
+			_putchar(a);
+		}
+		_putchar('\n');
+	}
+}
