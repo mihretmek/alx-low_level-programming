5b72948e3993de9e5c8b6716b57c70d5b09923b9
mihret_mekbebe <mihretasamenew2018@gmail.com>
2023-03-21 03:15:41 -0700

    1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -10,7 +10,10 @@ void print_alphabet(void)
 	char a;
 
 	for (a = 'a'; a <= 'z'; a++)
+	{
 		_putchar(a);
+	}
+
 	_putchar('\n');
 
 	return(0);
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,24 +1,23 @@
-#ifndef _MAIN_H_
-#define _MAIN_H_
-
-
-/**
- * _putchar - prints characters
- */
-
-int _putchar(char a)
-/**
- * print_alphabet - print alphabet function
- */
+int _putchar(char c);
 
 void print_alphabet(void);
 
-/**
- * print_alphabet_x10 - print alphabet 10 times function
- */
-
 void print_alphabet_x10(void);
 
+int _islower(int c);
 
-#endif
+int _isalpha(int c);
 
+int print_sign(int n);
+
+int _abs(int);
+
+int print_last_digit(int);
+
+void jack_bauer(void);
+
+void times_table(void);
+
+int add(int, int);
+
+void print_to_98(int n);
