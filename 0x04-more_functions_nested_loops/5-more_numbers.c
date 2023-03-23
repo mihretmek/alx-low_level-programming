3a1faa9d5a1957a9762379fbd2a589afed744d3b
mihret_mekbebe <mihretasamenew2018@gmail.com>
2023-03-23 13:14:54 -0700

    5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,13 +9,13 @@ void more_numbers(void)
 	int a;
 	int b;
 
-	for (b = 0; b < 10; b++)
+	for (a = 0; a < 10; a++)
 	{
-		for (a = 0; a <= 14; a++)
+		for (b = 0; b <= 14; b++)
 		{
-			if (a > 9)
-				_putchar(a / 10 + '0');
-			_putchar(a % 10 + '0');
+			if (b > 9)
+				_putchar(b / 10 + '0');
+			_putchar((b % 10) + '0');
 		}
 	}
 	_putchar('\n');
