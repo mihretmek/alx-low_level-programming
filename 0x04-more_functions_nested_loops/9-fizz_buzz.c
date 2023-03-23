240f64da3391d5bf3a427dd006b5c15f02a2e94d
mihret_mekbebe <mihretasamenew2018@gmail.com>
2023-03-23 13:22:29 -0700

    9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -8,24 +8,23 @@
 
 int main(void)
 {
-	int a = 1;
+	int a;
 
-	while (a <= 100)
+	for (a = 1; a < 100; a++)
 	{
-		if (a % 3 == 0 && a % 5 == 0)
+		if ((a % 3) == 0 && (a % 5) == 0)
 		{
-			printf("%s", "FizzBuzz ");
+			printf("FizzBuzz ");
 		} else if (a % 3 == 0)
 		{
-			printf("%s", "Fizz ");
+			printf("Fizz ");
 		} else if (a % 5 == 0)
 		{
-			printf("%s", "Buzz ");
+			printf("Buzz ");
 		} else
 		{
 			printf("%d ", a);
 		}
-		a++;
 	}
 	printf("\n");
 	return (0);
