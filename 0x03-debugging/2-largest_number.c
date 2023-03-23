9931422485bfd1ba73fdf973b597d4ecec94d172
mihret_mekbebe <mihretasamenew2018@gmail.com>
2023-03-23 14:51:48 -0700

    2

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,33 +12,18 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if (a < b)
+	if (a > b && a > c)
 	{
-		if (b > c)
-		{
-			largest = b;
-		} else if (b < c)
-		{
-			largest = c;
-		}
-	} else if (a < c)
+		largest = a;
+	} else if (a > b && c > a)
 	{
-		if (c > b)
-		{
-			largest = c;
-		} else if (c < b)
-		{
-			largest = b;
-		}
-	} else if (b < c)
+		largest = c;
+	} else if (b > c)
 	{
-		if (c > a)
-		{
-			largest = c;
-		} else if (c < a)
-		{
-			largest = a;
-		}
+		largest = b;
+	} else
+	{
+		largest = c;
 	}
 	return (largest);
 }
