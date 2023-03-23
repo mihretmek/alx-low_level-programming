8367c529e93d8966c90a74fbdc0780b26f3ecf29
mihret_mekbebe <mihretasamenew2018@gmail.com>
2023-03-23 13:07:39 -0700

    8

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -10,16 +10,14 @@ void print_square(int size)
 	int a;
 	int b;
 
-	if (size <= 0)
-	{
-		_putchar('\n');
-	} else
+	if (size > 0)
 	{
 		for (a = 0; a < size; a++)
 		{
 			for (b = 0; b < size; b++)
 				_putchar('#');
+			_putchar('\n');
 		}
+	} else
 		_putchar('\n');
-	}
 }
