1a3edcb7e1b830e0c40493f1cbe29cb6c2179946
mihret_mekbebe <mihretasamenew2018@gmail.com>
2023-03-28 11:22:41 -0700

    1

---
+++ 0x05-pointers_arrays_strings/1-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strlen - shows length of string
+ * Return: string length
+ * @s: parameter
+ */
+
+int _strlen(char *s)
+{
+	int n;
+
+	for (n = 0; s[n] != '\0'; n++)
+		return (n);
+}
