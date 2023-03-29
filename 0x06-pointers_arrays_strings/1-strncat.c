59c88be08a094ff56c1078e36a948907ae190c6f
mihret_mekbebe <mihretasamenew2018@gmail.com>
2023-03-29 09:44:05 -0700

    1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -16,7 +16,7 @@ char *_strncat(char *dest, char *src, int n)
 
 	for (j = 0; dest[j] != '\0'; j++)
 	{}
-	for (i = 0; i < (n && src[i]) != '\0'; i++)
+	for (i = 0; i < (n && src[i] != '\0'); i++)
 		dest[j + i] = src[i];
 	dest[j + i] = '\0';
 	return (dest);
