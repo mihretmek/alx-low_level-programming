9b62dac426e6817f698cf8261531b8bbb7ad25c2
mihret_mekbebe <mihretasamenew2018@gmail.com>
2023-03-29 11:35:08 -0700

    2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -12,9 +12,11 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
-	for (i = 0; i < n && src[i] != '\0'; i++)
+	i = 0;
+
+	for (; i < n && src[i] != '\0'; i++)
 		dest[i] = src[i];
-	for (i = 0; i < n; i++)
+	for (; i < n; i++)
 		dest[i] = '\0';
 	return (dest);
 }
