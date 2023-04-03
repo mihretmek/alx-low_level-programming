5a4ee646386f2f659e3173c700eba3d4031c1cd5
mihret_mekbebe <mihretasamenew2018@gmail.com>
2023-04-03 02:50:00 -0700

    2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * _strchr - a function that locates a character in a string
@@ -14,7 +15,9 @@ char *_strchr(char *s, char c)
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		if (s[i] == c)
+		{
 			return (s + i);
+		}
 	}
 	if (s[i] == c)
 		return (s + i);
