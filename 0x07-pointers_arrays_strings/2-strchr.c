c05b49986dda30df00e646ce01f678ba7dd68c71
mihret_mekbebe <mihretasamenew2018@gmail.com>
2023-04-03 02:11:41 -0700

    2 modified

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -15,7 +15,10 @@ char *_strchr(char *s, char c)
 	{
 		if (s[i] == c)
 			return (s + i);
-		else if (s[i] == 0)
+		else if (s[i] == '\0')
 			return (NULL);
 	}
+	if (s[i] == c)
+		return (s + i);
+	return (0);
 }
