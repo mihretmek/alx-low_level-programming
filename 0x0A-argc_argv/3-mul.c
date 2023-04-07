af7c54beec29b26541b3b4d05bda42eae8206d6c
mihret_mekbebe <mihretasamenew2018@gmail.com>
2023-04-07 11:18:00 -0700

    arguments

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,66 +1,28 @@
 #include <stdio.h>
-#include "main.h"
+#include <stdlib.h>
 
-/**
- * _atoi - converts a string to an integer
- * @s: string to be converted
- *
- * Return: the int converted from the string
- */
-int _atoi(char *s)
-{
-	int i, d, n, len, f, digit;
-
-	i = 0;
-	m = 0;
-	n = 0;
-	len = 0;
-	a = 0;
-	b = 0;
-
-	while (s[len] != '\0')
-		len++;
-
-	while (i < len && a == 0)
-	{
-		if (s[i] == '-')
-			++m;
-
-		if (s[i] >= '0' && s[i] <= '9')
-		{
-			b = s[i] - '0';
-			if (m % 2)
-				b = -b;
-			n = n * 10 + b;
-			f = 1;
-			if (s[i + 1] < '0' || s[i + 1] > '9')
-				break;
-			a = 0;
-		}
-		i++;
-	}
-
-	if (a == 0)
-		return (0);
-
-	return (n);
-}
 
 /**
  * main - prints product
- * Return: 1
+ * Return: 0 or 1
  * @argc: number of arguments
  * @argv: array of arguments
  */
 
 int main(int argc, char *argv[])
 {
-	if (argc == 3)
+	int num1;
+	int num2;
+	int product;
+
+	if (argc != 3)
 	{
-		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
-		return (0);
-	}
-	else
 		printf("Error\n");
-	return (1);
+		return (1);
+	}
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+	product = num1 * num2;
+	printf("%d\n", product);
+	return (0);
 }
