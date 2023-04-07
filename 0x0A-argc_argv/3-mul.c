531054cf7f71b31cb92a1536fb1ced01d378d1f0
mihret_mekbebe <mihretasamenew2018@gmail.com>
2023-04-07 10:24:50 -0700

    arguments

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,51 +1,6 @@
 #include <stdio.h>
+#include <stdlib>
 #include "main.h"
-
-/**
- * _atoi - converts string to int
- * Return: integer
- * @s: string
- */
-
-int _atoi(char *s)
-{
-	int i;
-	int n;
-	int len;
-	int a;
-	int b;
-	int c;
-
-	i = 0;
-	n = 0;
-	len = 0;
-	a = 0;
-	b = 0;
-	c = 0;
-	while (s[len] != '\0')
-		len++;
-	while (i < len && c == 0)
-	{
-		if (s[i] == '-')
-			++b;
-		if (s[i] >= '0' && s[i] <= '9')
-		{
-			a = s[i] - '0';
-			if (b % 2)
-				a = -a;
-			n = n * 10 + a;
-			c = 1;
-			if (s[i + 1] < '0' || s[i + 1] > '9')
-				break;
-			c = 0;
-		}
-		i++;
-	}
-	if (c == 0)
-		return (0);
-	return (n);
-
-}
 
 /**
  * main - prints product
