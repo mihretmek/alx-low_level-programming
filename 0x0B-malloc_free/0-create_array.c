4c2a351488036f59599c55cb8029122db7753a4d
mihret_mekbebe <mihretasamenew2018@gmail.com>
2023-04-10 01:21:22 -0700

    malloc and free

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -17,11 +17,12 @@ char *create_array(unsigned int size, char c)
 	if (size <= 0)
 		return (NULL);
 
-	p = malloc(sizeof(char));
+	p = malloc(sizeof(char) * size);
 
 	if (p == '\0')
 		return (NULL);
 	for (i = 0; i < size; i++)
 		c[i] = p;
+	
 	return (p);
 }
