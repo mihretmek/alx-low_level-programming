6478b12059f8dc865a31baf3f226d91ae0692bfd
mihret_mekbebe <mihretasamenew2018@gmail.com>
2023-04-17 13:13:04 -0700

    structures

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,5 @@
-#ifndef _DOG_H_
-#define _DOG_H_ 
+#ifndef DOG_H
+#define DOG_H 
 
 /**
  * struct dog - structure of dog
@@ -15,18 +15,8 @@ struct dog
 	char *owner;
 };
 
-/**
- * dog_t - typedef for dog
- */
-
-typedef struct dog dog_t;
-
 void init_dog(struct dog *d, char *name, float age, char *owner);
 
 void print_dog(struct dog *d);
-
-dog_t *new_dog(char *name, float age, char *owner);
-
-void free_dog(dog_t *d);
 
 #endif
