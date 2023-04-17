9a10149968daf378a75c572005a413c2c3ef7dd6
mihret_mekbebe <mihretasamenew2018@gmail.com>
2023-04-17 11:41:35 -0700

    preprocessor

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - a function to print name of file compiled from
+ * Return: 0
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
