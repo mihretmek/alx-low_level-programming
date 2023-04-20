01c85300d0da95650827587bbaa3921587926304
mihret_mekbebe <mihretasamenew2018@gmail.com>
2023-04-20 02:54:06 -0700

    variadic functions

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,8 +1,64 @@
 #include "variadic_functions.h"
-#include "format.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
+
+/**
+ * print_char - prints character
+ * @n: list
+ * Return: 0;
+ */
+
+int print_char(va_list n)
+{
+	printf("%c", va_arg(n, int));
+	return (0);
+}
+
+/**
+ * print_int - prints integer
+ * @n: list
+ * Return: 0;
+ */
+
+int print_int(va_list n)
+{
+	printf("%d", va_arg(n, int));
+	return (0);
+}
+
+/**
+ * print_float - prints float
+ * @n: list
+ * Return: 0;
+ */
+
+int print_float(va_list n)
+{
+	printf("%f", va_arg(n, double));
+	return (0);
+}
+
+/**
+ * print_str - prints string
+ * @n: list
+ * Return: 0;
+ */
+
+int print_str(va_list n)
+{
+	char *s;
+
+	s = va_arg(n, char *);
+
+	if (s == NULL)
+	{
+		printf("(nil)");
+		return (0);
+	}
+	printf("%s", s);
+	return (0);
+}
 
 /**
  * print_all - a function that prints everything
